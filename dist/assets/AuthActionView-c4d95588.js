998d12304c55cbb9939a0c8d2296fd5b328a0f25
Kemal Bećarević <116683338+becarevicKemal22@users.noreply.github.com>
2023-10-29 20:06:18 +0100

    fix: fixed note title triggering save event on blur

---
+++ dist/assets/AuthActionView-c4d95588.js
@@ -0,0 +1,1 @@
+import{q as e,o as c,c as t}from"./index-62d6cd1e.js";const o={};function n(r,s){return c(),t("div")}const _=e(o,[["render",n]]);export{_ as default};
--- src/components/TextEditor/TextEditorNoteTitle.vue
+++ src/components/TextEditor/TextEditorNoteTitle.vue
@@ -1,4 +1,4 @@
-<script setup lang="ts">
+<script lang="ts" setup>
 
 import {computed, onMounted, onUpdated, ref, watch} from "vue";
 import {useNoteStore} from "@/stores/noteStore";
@@ -6,46 +6,47 @@ import {useNoteStore} from "@/stores/noteStore";
 const noteStore = useNoteStore();
 
 const noteTitle = computed(() => {
-  return noteStore.selectedNote?.title;
+    return noteStore.selectedNote?.title;
 });
 
 const elInput = ref<HTMLInputElement | null>(null);
 const update = () => {
-  noteStore.setSelectedNoteTitle((elInput.value as HTMLInputElement).value, false) ;
+    noteStore.setSelectedNoteTitle((elInput.value as HTMLInputElement).value, false);
 }
 
 const blurInput = () => {
-  (elInput.value as HTMLInputElement).blur();
-  noteStore.setSelectedNoteTitle((elInput.value as HTMLInputElement).value, true);
+    (elInput.value as HTMLInputElement).blur();
+    noteStore.setSelectedNoteTitle((elInput.value as HTMLInputElement).value, true);
+}
+
+const handleUpdate = () => {
+    if (noteTitle.value === 'New note') {
+        (elInput.value as HTMLInputElement).focus();
+        (elInput.value as HTMLInputElement).select();
+    }
 }
 
 onUpdated(() => {
-  if(noteTitle.value === 'New note'){
-    (elInput.value as HTMLInputElement).focus();
-    (elInput.value as HTMLInputElement).select();
-  }
+    handleUpdate();
 });
 onMounted(() => {
-  if(noteTitle.value === 'New note'){
-    (elInput.value as HTMLInputElement).focus();
-    (elInput.value as HTMLInputElement).select();
-  }
+    handleUpdate();
 });
 
 </script>
 
 <template>
-  <div class="font-title w-full text-xl lg:text-3xl text-gray-600 border-b-2 border-gray-200 focus-within:border-gray-300">
-    <input
-        type="text"
-        ref="elInput"
-        :value="noteTitle"
-        @input="update"
-        @keydown.enter="blurInput"
-        @blur="blurInput"
-        class="w-full rounded-md rounded-b-none caret-primary-light p-4 pb-2 focus:pb-3 focus:outline-0 focus:bg-gray-100 transition-all ease-out"
-    >
-  </div>
+    <div class="font-title w-full text-xl lg:text-3xl text-gray-600 border-b-2 border-gray-200 focus-within:border-gray-300">
+        <input
+                ref="elInput"
+                :value="noteTitle"
+                class="w-full rounded-md rounded-b-none caret-primary-light p-4 pb-2 focus:pb-3 focus:outline-0 focus:bg-gray-100 transition-all ease-out"
+                type="text"
+                @blur="blurInput"
+                @input="update"
+                @keydown.enter="blurInput"
+        >
+    </div>
 </template>
 
 <style scoped>
